d4efe45f581a6b59ca610a0b67b03e32662a1e02
DevAdmin <hanhpth0207@gmail.com>
2022-09-15 22:41:59 +0700

    Update wait seconds

--- src/component/Update/UpdateBook.js
+++ src/component/Update/UpdateBook.js
@@ -57,6 +57,7 @@ function UpdateBook(){
                 const status = response.status
                 if (status === 200)
                 {
+                    setTimeout('', 2000);
                     alert("Book update successfull")
                     redirectPage();
                 }
